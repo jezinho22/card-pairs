9985583eb535349ac5d6e2a477d40e31a6833f4a
Jeremy Johns <jez@TechEd-0019.techeducators.co.uk>
2023-05-21 12:20:11 +0100

    added data file and rendered cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import teamData from "./resources/data.json"
 
 function App() {
+  console.log(teamData[0])
   return (
     <div className="App">
       <h1>
 Card Pairs Game
       </h1>
+
+      {teamData.map((member)=>{
+        return <div>
+          <p>{member.name}</p>
+        <img src={member.image_url} alt={member.name}></img>
+        </div>
+      })}
+      
     </div>
   );
 }
