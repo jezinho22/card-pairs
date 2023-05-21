00e5a02dabf4cf848a137ad65b5cf7ade312a603
Jeremy Johns <jez@TechEd-0019.techeducators.co.uk>
2023-05-21 21:06:41 +0100

    added backs choices and logic

--- src/App.js
+++ src/App.js
@@ -4,19 +4,60 @@ import teamData from "./resources/data.json"
 import Card from './components/Card';
 
 function App() {
-  const [cards, setcards] = useState([])
-function setUpCards(){
-  const cards = teamData;
-  const cardsSplit = teamData.map((obj)=> {
-  return ([{...obj, pair:"image"},{...obj, pair:"text"}])})
-  const cardsCombined = cardsSplit.reduce((combined, arr)=>[...combined, ...arr])
-  const cardsShuffled = cardsCombined.sort((a,b) => 0.5 - Math.random())
-  setcards(cardsShuffled)
+  const [cards, setCards] = useState([])
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [tries, setTries] = useState(0)
+
+// add card to state of choice one, or if it's full then choice two
+  function handleClick(card){
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+
   }
+
+// whenever a card is clicked, check if one and two match
+// and reset for next turn
+useEffect(() => {
+  if (choiceOne && choiceTwo){
+    if(choiceOne.name === choiceTwo.name){
+      console.log(choiceOne.name)
+      // previously is the previous state for cards
+      // setCards will set to previous state withs something done to it
+      setCards(previously => {
+        return previously.map(card =>{
+          // there are two cards which match choiceOne.name
+          // this will change only those two to say matched
+          if(card.name === choiceOne.name){
+          return {...card, matched:true}
+        }})
+      })
+      tryAgain()
+    } else {
+      console.log("Uh uh uh. No match there!")
+      tryAgain()
+    }
+  }
+}, [choiceOne, choiceTwo])
+
+
+// reset for next try
+function tryAgain(){
+  setTries(tries + 1)
+  setChoiceOne(null)
+  setChoiceTwo(null)
+}
+
+
+  function setUpCards(){
+    const cards = teamData;
+    const shuffledCards = teamData
+      .map((obj)=> [{...obj, pair:"image", id:Math.random(), matched:false},
+                    {...obj, pair:"text", id:Math.random(), matched:false}])
+      .reduce((combined, arr)=>[...combined, ...arr])
+      .sort((a,b) => 0.5 - Math.random())
+    setCards(shuffledCards)
+    }
   
-  useEffect(() => {
-setUpCards()
-  }, [])
 
 //   function renderCard(card){
 // if(card.pair == "image"){
@@ -29,10 +70,14 @@ setUpCards()
       <h1>
 Card Pairs Game
       </h1>
+      <button onClick={setUpCards}/>
+      <div className = "card-display-area">
+
     {cards.map((card)=>{
-      return <Card card={card} />
+      return <Card card={card} handleClick={handleClick} />
     })}
-      
+    
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,16 @@
 import "../css/Card.css"
+import logo from "../resources/TECHED_LOGO.png"
 
-function Card({card}) {
+function Card({card, handleClick}) {
   return (
-    <div className="Card" name={card.name}>
-        { card.pair == "image" && <img src={card.image_url} alt={card.name}></img>}
+    <div className="Card" key={card.id} name={card.name}>
+        <div className = "cardFront">
+        {card.pair == "image" && <img src={card.image_url} alt={card.name}></img>}
         {card.pair == "text" && <p>{card.name}</p>}
-
+        </div>
+        <div className="cardBack">
+        <img src={logo} alt="back of card" onClick={()=>handleClick(card)}></img>
+    </div>
     </div>
   );
 }
