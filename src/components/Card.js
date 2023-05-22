9d714233dad44a3ac450fb6328897f5ad06c73c2
Jeremy Johns <jez@TechEd-0019.techeducators.co.uk>
2023-05-23 00:06:40 +0100

    adding flip css

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,17 @@ import logo from "../resources/TECHED_LOGO.png"
 function Card({card, handleClick, flipped}) {
   return (
     <div className="Card" key={card.id} name={card.name}>
-
-      <div className = "cardFront">
-        {card.pair == "image" && <img src={card.image_url} alt={card.name}></img>}
-        {card.pair == "text" && <p>{card.name}</p>}
-       </div>
-       <div className="cardBack">
-        <img src={logo} alt="back of card" onClick={()=>handleClick(card)}></img>
+      <div className={flipped ? "flipped" : ""}>
+        
+        <div className = "cardFront">
+          {card.pair == "image" && <img src={card.image_url} alt={card.name}></img>}
+          {card.pair == "text" && <p>{card.name}</p>}
+        </div>
+        
+        <div className="cardBack">
+          <img src={logo} alt="back of card" onClick={()=>handleClick(card)}></img>
+        </div>
+        
       </div>
     </div>
   );
